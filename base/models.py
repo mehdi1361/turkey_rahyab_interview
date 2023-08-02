49863af3a98b968677ba63b3309528764e91a68b
mehdi <mhd.mosavi@gmail.com>
2023-08-03 01:28:45 +0330

    first commit

---
+++ base/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Base(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
