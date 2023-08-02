9df80f3cd605a8c8ebac0ec9b973e6ee79722c05
mehdi <mhd.mosavi@gmail.com>
2023-08-03 02:47:13 +0330

    add models

--- post/models.py
+++ post/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 from base.models import Base
+from user.models import UserAccount
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+
+class Announcement(Base):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    owner = models.ForeignKey(UserAccount, null=True, on_delete=models.SET_NULL, related_name="announcements")
+    picture = models.ImageField(null=True, upload_to="images/", blank=True, verbose_name="Category picture")
+    medium_picture = ImageSpecField(source='picture', processors=[ResizeToFill(150, 150)], format="PNG",
+                                      options={'quality': 80})
+
+    small_picture = ImageSpecField(source='picture', processors=[ResizeToFill(80, 80)], format="PNG",
+                                      options={'quality': 80})
+    class Meta:
+        db_table = "announcement"
--- turkey_rahyab_interview/settings.py
+++ turkey_rahyab_interview/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'post'
+    'user',
+    'post',
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +125,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = "user.UserAccount"
